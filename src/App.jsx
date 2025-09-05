340170567267c4f6284b445f19fa26b19cac03fe
rez-ahmed <dewanrezwanahmed@gmail.com>
2025-09-05 11:24:37 +0600

    completed creating all routes inside App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Collection from './pages/Collection';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import PlaceOrder from './pages/PlaceOrder';
+import Orders from './pages/Orders';
 
 const App = () => {
   return (
@@ -11,8 +18,13 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
-        <Route />
-        <Route />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/order' element={<Orders />} />
       </Routes>
     </div>
   )
